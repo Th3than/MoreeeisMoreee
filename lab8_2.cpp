6fb1a0c72da19599e0b093fa54a66cbb9c245d77
Natan <th3than@gmail.com>
2022-12-20 16:17:00 +0700

    lab8-2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -16,3 +16,25 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+#include <iostream>
+#include<string>
+using namespace std;
+int i,id;
+string name , movie, Next, bye;
+int main() {
+    cout <<"Fahsai: Sawadee ka...Can you tell me your name?";
+    cout << "\n?????: ";
+    getline(cin,name);
+    cout <<"Fahsai: Wow!!! "<<name<<" is a really cool name.\nFahsai: I think you are an Engineering student. What is your student ID?\n"<<name<<": ";
+    cin >> id;
+    cin.ignore();
+    i = id/10000000-12;
+    cout <<"Fahsai: I think you may be GEAR "<< i <<". I have a free movie ticket for you.\nFahsai: Let's go to the cinema together!!!\nFahsai: What movie do you want to watch?\n";
+    cout << name<<": ";
+    getline(cin,movie);
+    cout<<"Fahsai: So....which day are you free to go with me?\n"<<name<<": ";
+    getline(cin,Next);
+    cout<<"Fahsai: "<<Next<<"....that is OK!!! I'm looking forward to watching "<<movie<<" with you.\n"<<name<<": ";
+    cin >> bye;
+    cout << "Fahsai: 555+ see you "<<Next<<". Bye Bye \\(^ ^)/";
+}
